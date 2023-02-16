684e7b7ea7cd7465b5f0b72ab15d0dc86fad46d4
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2023-02-16 05:22:17 +0100

    scripttool: Fix typo in license

--- scripttool/include/ms_groupfile.hpp
+++ scripttool/include/ms_groupfile.hpp
@@ -1,4 +1,4 @@
-// Copyright 2023 Michael Fabian Dirks <info@xamyar.com>
+// Copyright 2023 Michael Fabian Dirks <info@xaymar.com>
 //
 // Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 //
